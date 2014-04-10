95016b5475ec4e9c930c1d5541d4c7da2b69f151
Abhinav Jhanwar <abhinav.jhanwar.august2@gmail.com>
2014-04-10 12:13:59 +0530

    Update and rename common.mk to gapps.mk

--- gapps.mk
+++ gapps.mk
@@ -1,6 +1,6 @@
 PRODUCT_COPY_FILES += \
-    vendor/google/gapps/system/addon.d/70-gapps.sh:system/addon.d/70-gapps.sh \
     vendor/google/gapps/system/app/GoogleContactsSyncAdapter.apk:system/app/GoogleContactsSyncAdapter.apk \
+    vendor/google/gapps/system/addon.d/70-gapps.sh:system/addon.d/70-gapps.sh \
     vendor/google/gapps/system/etc/permissions/com.google.android.maps.xml:system/etc/permissions/com.google.android.maps.xml \
     vendor/google/gapps/system/etc/permissions/com.google.android.media.effects.xml:system/etc/permissions/com.google.android.media.effects.xml \
     vendor/google/gapps/system/etc/permissions/com.google.widevine.software.drm.xml:system/etc/permissions/com.google.widevine.software.drm.xml \
@@ -15,12 +15,12 @@ PRODUCT_COPY_FILES += \
     vendor/google/gapps/system/lib/libgcastv2_support.so:system/lib/libgcastv2_support.so \
     vendor/google/gapps/system/lib/libjgcastservice.so:system/lib/libjgcastservice.so \
     vendor/google/gapps/system/lib/libjni_latinime.so:system/lib/libjni_latinime.so \
-    vendor/google/gapps/system/priv-app/GoogleBackupTransport.apk:system/app/GoogleBackupTransport.apk \
-    vendor/google/gapps/system/priv-app/GoogleFeedback.apk:system/app/GoogleFeedback.apk \
-    vendor/google/gapps/system/priv-app/GoogleLoginService.apk:system/app/GoogleLoginService.apk \
-    vendor/google/gapps/system/priv-app/GoogleOneTimeInitializer.apk:system/app/GoogleOneTimeInitializer.apk \
-    vendor/google/gapps/system/priv-app/GooglePartnerSetup.apk:system/app/GooglePartnerSetup.apk \
-    vendor/google/gapps/system/priv-app/GoogleServicesFramework.apk:system/app/GoogleServicesFramework.apk \
-    vendor/google/gapps/system/priv-app/Phonesky.apk:system/app/Phonesky.apk \
-    vendor/google/gapps/system/priv-app/PrebuiltGmsCore.apk:system/app/PrebuiltGmsCore.apk \
-    vendor/google/gapps/system/priv-app/SetupWizard.apk:system/app/SetupWizard.apk
+    vendor/google/gapps/system/priv-app/GoogleBackupTransport.apk:system/priv-app/GoogleBackupTransport.apk \
+    vendor/google/gapps/system/priv-app/GoogleFeedback.apk:system/priv-app/GoogleFeedback.apk \
+    vendor/google/gapps/system/priv-app/GoogleLoginService.apk:system/priv-app/GoogleLoginService.apk \
+    vendor/google/gapps/system/priv-app/GoogleOneTimeInitializer.apk:system/priv-app/GoogleOneTimeInitializer.apk \
+    vendor/google/gapps/system/priv-app/GooglePartnerSetup.apk:system/priv-app/GooglePartnerSetup.apk \
+    vendor/google/gapps/system/priv-app/GoogleServicesFramework.apk:system/priv-app/GoogleServicesFramework.apk \
+    vendor/google/gapps/system/priv-app/Phonesky.apk:system/priv-app/Phonesky.apk \
+    vendor/google/gapps/system/priv-app/PrebuiltGmsCore.apk:system/priv-app/PrebuiltGmsCore.apk \
+    vendor/google/gapps/system/priv-app/SetupWizard.apk:system/priv-app/SetupWizard.apk
