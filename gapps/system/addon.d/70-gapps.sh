6a63b78f829e906d9afcd66f3a998074c05df2b7
95A31 <x95a31xreg@gmail.com>
2014-09-02 12:31:47 +0200

    Update gApps

--- gapps/system/addon.d/70-gapps.sh
+++ gapps/system/addon.d/70-gapps.sh
@@ -7,20 +7,27 @@
 list_files() {
 cat <<EOF
 app/GoogleContactsSyncAdapter.apk
+etc/permissions/com.google.android.ble.xml
+etc/permissions/com.google.android.camera2.xml
 etc/permissions/com.google.android.maps.xml
 etc/permissions/com.google.android.media.effects.xml
 etc/permissions/com.google.widevine.software.drm.xml
 etc/permissions/features.xml
 etc/preferred-apps/google.xml
+framework/com.google.android.ble.jar
+framework/com.google.android.camera2.jar
 framework/com.google.android.maps.jar
 framework/com.google.android.media.effects.jar
 framework/com.google.widevine.software.drm.jar
 lib/libAppDataSearch.so
+lib/libconscrypt_gmscore_jni.so
 lib/libgames_rtmp_jni.so
 lib/libgcastv2_base.so
 lib/libgcastv2_support.so
+lib/libgmscore.so
 lib/libjgcastservice.so
 lib/libjni_latinime.so
+lib/libocrclient.so
 priv-app/GoogleBackupTransport.apk
 priv-app/GoogleFeedback.apk
 priv-app/GoogleLoginService.apk
@@ -56,17 +63,13 @@ case "$1" in
     # Stub
   ;;
   post-restore)
-   rm -f /system/app/QuickSearchBox.apk
-   rm -f /system/app/GmsCore.apk
-   rm -f /system/app/Velvet.apk
-   rm -f /system/app/Vending.apk
    rm -f /system/app/BrowserProviderProxy.apk
    rm -f /system/app/PartnerBookmarksProvider.apk
-   rm -f /system/priv-app/QuickSearchBox.apk
-   rm -f /system/priv-app/GmsCore.apk
-   rm -f /system/priv-app/Velvet.apk
-   rm -f /system/priv-app/Vending.apk
+   rm -f /system/app/Provision.apk
+   rm -f /system/app/Vending.apk
    rm -f /system/priv-app/BrowserProviderProxy.apk
-   rm -f /system/priv-app/PartnerBookmarksProvider.apk 
+   rm -f /system/priv-app/PartnerBookmarksProvider.apk
+   rm -f /system/priv-app/Provision.apk
+   rm -f /system/priv-app/Vending.apk
   ;;
 esac
